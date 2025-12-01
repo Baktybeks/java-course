5ff37e63d2c4abf17c0879c4738e4acc9657eda8
Baktybeks <baktybeks@mail.ru>
2025-12-01 17:59:44 +0600

    Java

---
+++ src/Main01.java
@@ -0,0 +1,28 @@
+public class Main01 {
+    public static void main(String[] args) {
+
+        //Коментарий
+
+        /*
+            многострочный
+            коментарий
+         */
+        System.out.print("Я люблю пиццу\n");
+        System.out.println("Это хорошо\n");
+        System.out.println("Hello World\n");
+        //sout tab
+
+        // ❎ variable = A reusable container for a value.
+        //                          A variable behaves as if it was the value it contains.
+
+        // 🟥 Primitive = simple value stored directly in memory (stack)
+        // 🟦 Reference = memory address (stack) that points to the (heap)
+
+        // 🟥 Primitive vs 🟦 Reference
+        //    ---------                      ---------
+        //    int                           string
+        //    double                    array
+        //    char                        object
+        //    boolean
+    }
+}
