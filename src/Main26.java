edaf09a06d3af2b0575cad343f82b09a526fb26f
Baktybeks <baktybeks@mail.ru>
2026-01-01 20:53:05 +0600

    add  26-27

--- src/Main26.java
+++ src/Main26.java
@@ -1,4 +1,26 @@
 public class Main26 {
+    public static void main(String[] args){
 
-    
+        // method =  a block of reusable code that is executed when called ()
+
+        String  name =  "Spongebob";
+        int age = 30;
+
+        happyBirthday(name, age);
+    }
+    static void happyBirthday(String name, int age){
+        System.out.println("Happy Birthday to you!");
+        System.out.printf("Happy Birthday dear %s!\n", name);
+        System.out.printf("You are %d years old!\n", age);
+        System.out.println("Happy Birthday to you!\n");
+    }
+    static double square(double number){
+        return number * number;
+    }
+    static double cube(double number){
+        return number * number * number;
+    }
+    static String getFullName(String first, String last){
+        return first + " " + last;
+    }
 }
